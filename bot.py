83edc551dee9814d56b19d75acb7b58722260064
wgam951-ux <wgam951@gmail.com>
2025-09-22 03:50:39 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+def start(update, context):
+    update.message.reply_text("🤖 البوت شغال بنجاح!")
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
